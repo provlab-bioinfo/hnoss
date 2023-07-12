823f44eaa001d96faeaf9d1cc2cde51847dee788
knacko <geekbike@hotmail.com>
2023-07-12 14:40:30 -0600

    feat: added summarize script

---
+++ analysis/summ_all.R
@@ -0,0 +1,45 @@
+library(openxlsx)
+library(tidyverse)
+library(magrittr)
+library(rio)
+library(rlang)
+
+xlsx <- import("J:\\virus_covid19\\ww_covid\\mixed_samples_big_BN.csv")
+summCols <- xlsx %>% select(A_x:Zeta) %>% colnames() 
+lineCols <- xlsx %>% select(A_y:XZ) %>% colnames()
+
+summ <- xlsx %>% filter(coverage_x > 80)
+summ %<>% mutate(Max.Lineage=pmax(!!!rlang::syms(lineCols),na.rm=TRUE))
+summ %<>% filter(Max.Lineage < 95)
+all_na <- function(x) any(!is.na(x))
+summ %<>% select_if(all_na)
+
+df <- summ
+
+max_list <- function(x) {
+  max <- x[!is.na(x)]
+  #max <- as.list(x %<>% select_if(~ !any(is.na(.))))
+  max <- head(max[order(max,decreasing=TRUE)],3)
+  #max <- max[order(names(max),decreasing=TRUE)][1:3]
+  cols <- names(max)
+  vals <- unlist(unname(max))
+
+  out <- mapply(paste, cols, vals,  MoreArgs = list(sep = ";"))
+  out <- paste(out,collapse=";")
+  return (out)
+}
+
+summCols <- intersect(summCols, colnames(df))
+lineCols <- intersect(lineCols, colnames(df))
+
+
+df['SummStats'] = apply(df[summCols],1,max_list)
+df['LineStats'] = apply(df[lineCols],1,max_list)
+
+out <- df
+
+out %<>% separate_wider_delim(SummStats, ";", names = c("Summ. 1", "Summ. 1 (%)","Summ. 2", "Summ. 2 (%)","Summ. 3", "Summ. 3 (%)"), too_few = "align_start")
+out %<>% separate_wider_delim(LineStats, ";", names = c("Line. 1", "Line. 1 (%)","Line. 2", "Line. 2 (%)","Line. 3", "Line. 3 (%)"), too_few = "align_start")
+
+write.xlsx(out,"J:\\virus_covid19\\ww_covid\\mixed_samples_summarized2.xlsx")
+
