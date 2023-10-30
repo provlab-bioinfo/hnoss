e6ec754dd0acc2f5e3260c3dd1f9261ae511b447
knacko <geekbike@hotmail.com>
2023-10-30 14:00:43 -0600

    chore: more package compilation stuff

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ setup(
     name='hnoss',
     version='0.1.0-alpha',
     packages=find_namespace_packages(),
-    entry_points={
-    },
-    scripts=[],
-    package_data={
-    },
-    install_requires=[
-    ],
+    package_dir={"hnoss": "src"},
     description='Hnoss is an extension of the Freyja SARS-CoV-2 strain deconvolution package to allow for easier manipulation and more flexibility.',
     url='https://github.com/provlab-bioinfo/hnoss',
     author='Andrew Lindsay',
     author_email='andrew.lindsay@albertaprecisionlabs.ca',
     include_package_data=True,
     keywords=[],
-    zip_safe=False
+    zip_safe=False,
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'pango_aliasor',
+        'scipy',
+        'setuptools',
+        'statsmodels'
+    ]
 )
--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,5 @@
 import os, pandas as pd, subprocess, numpy as np
-import configSettings as cfg
+import src.configSettings as cfg
 from pathlib import Path
 from datetime import datetime, date
 from json import loads, dumps
@@ -406,6 +406,9 @@ def importToDataFrame(filename, **kargs):
             df = filename
     
     return df
+
+def test():
+    print ("blah")
 
 #endregion
 
